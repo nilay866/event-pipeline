7d21a5b2cd75f2bde68341321b15862b5b141d6a
nilay866 <124528996+nilay866@users.noreply.github.com>
2025-12-30 09:46:33 +0530

    Refactor SQS queue resources and names

--- terraform/terraform/sqs.tf
+++ terraform/terraform/sqs.tf
@@ -1,19 +1,13 @@
-# -------------------------
-# Dead Letter Queue (DLQ)
-# -------------------------
-resource "aws_sqs_queue" "data_dlq" {
-  name = "event-data-dlq"
+resource "aws_sqs_queue" "dlq" {
+  name = "tf-event-data-dlq"
 }
 
-# -------------------------
-# Main SQS Queue
-# -------------------------
-resource "aws_sqs_queue" "data_queue" {
-  name                       = "event-data-queue"
+resource "aws_sqs_queue" "main" {
+  name                      = "tf-event-data-queue"
   visibility_timeout_seconds = 60
 
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.data_dlq.arn
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = 5
   })
 }
