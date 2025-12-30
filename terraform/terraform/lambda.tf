407c9c710a176a17d821f59d5b0c558db457c3f6
root <root@ip-172-31-20-191.ap-south-1.compute.internal>
2025-12-30 10:52:59 +0000

    Fix Lambda handler and zip path

--- terraform/terraform/lambda.tf
+++ terraform/terraform/lambda.tf
@@ -1,8 +1,11 @@
 resource "aws_lambda_function" "daily_report" {
   function_name = "daily-report-generator"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
+  runtime       = "python3.10"
 
-  filename = "lambda.zip"  # reference only
+  handler = "lambda_function.lambda_handler"
+
+  filename         = "../lambda/lambda.zip"
+  source_code_hash = filebase64sha256("../lambda/lambda.zip")
 }
+
